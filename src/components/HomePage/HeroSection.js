70e2b3f497e80612f76e7d49d23fe883c2db550c
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2022-12-04 02:18:40 +0530

    interim

--- src/components/HomePage/HeroSection.js
+++ src/components/HomePage/HeroSection.js
@@ -1,28 +1,17 @@
+import Image from "next/image"
 import { useEffect, useState } from "react"
+import platform from "../../../public/assets/home/platform.svg";
+import heroBG from "../../../public/assets/home/heroBG.svg";
 
 export default function HeroSection(){
-
-    // const [checkedText, setCheckedText] = useState(0);
-
-    // useEffect(()=>{
-    //     setInterval(()=>{
-    //         // console.log(checkedText);
-    //         setCheckedText((prev)=>{console.log(prev); return (prev+1)%3});
-    //     }, 5000);
-    // },[])
-
     return (
-        <div className="flex items-center justify-center w-screen bg-[#E6D4FF]">
-            <div className="w-full max-w-[1920px] px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-36 h-screen max-h-[820px] flex flex-col items-center justify-center">
-                <h3 className="text-3xl font-semibold">Unlocking Infinite Possibilities with Web3</h3>
-                {/* <div className="hidden">
-                    <input name="checkboxes" type='radio' id="c1" readOnly checked={checkedText === 0}/>
-                    <input type='radio' name="checkboxes" id="c2" readOnly checked={checkedText === 1}/>
-                    <input type='radio' id="c3" name="checkboxes" readOnly checked={checkedText === 2}/>
-                </div> */}
-                <h1 className="text-[96px] font-semibold" id="check1">Built for People</h1>
-                {/* <h1 className="typed-out text-[96px] font-semibold" id="check2">Built for Innovators</h1>
-                <h1 className="typed-out text-[96px] font-semibold" id="check3">Built for Explorers</h1> */}
+        <div className="flex items-center justify-between w-screen bg-[#E6D4FF] flex-col">
+            <div className="w-full max-w-[1920px] pt-48 px-6 md:px-8 lg:px-16 xl:px-20 2xl:px-36 flex flex-col items-center justify-center">
+                <Image src={platform} alt="platform name"/>
+                <h1 className="font-semibold text-[#434343] font-primary text-[69px] mt-3">Where Ideas Never Die</h1>
+            </div>
+            <div className="w-full">
+                <Image src={heroBG} layout="responsive" priority alt="bg art"/>
             </div>
         </div>
     )
